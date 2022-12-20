71df968ab108cccc47e5b706fc18a3c0debc34df
davidkimgba <44686023+davidkimgba@users.noreply.github.com>
2022-12-20 05:03:25 -0800

    testing style  visibility vs display

--- functionalScripts.js
+++ functionalScripts.js
@@ -4,3 +4,16 @@ function dropdownFunction()
     console.log("test");
 
 }
+function toggleDropdown()
+{
+    if(document.getElementsByClassName("dropdowncontent")[0].style.display =="initial")
+    {
+        document.getElementsByClassName("dropdowncontent")[0].style.display = "none";
+    }
+    else
+    {
+        document.getElementsByClassName("dropdowncontent")[0].style.display ="initial";
+    }
+    console.log("test");
+
+}
